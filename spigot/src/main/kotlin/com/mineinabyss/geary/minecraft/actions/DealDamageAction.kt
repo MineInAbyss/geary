5c67d36246a706a22d9af65950b18aba315b9b50
0ffz <dan.voznyy@gmail.com>
2021-02-12 16:03:01 -0500

    Change damage action to take range

--- spigot/src/main/kotlin/com/mineinabyss/geary/minecraft/actions/DealDamageAction.kt
+++ spigot/src/main/kotlin/com/mineinabyss/geary/minecraft/actions/DealDamageAction.kt
@@ -3,6 +3,9 @@ package com.mineinabyss.geary.minecraft.actions
 import com.mineinabyss.geary.ecs.GearyEntity
 import com.mineinabyss.geary.ecs.actions.GearyAction
 import com.mineinabyss.geary.minecraft.components.toBukkit
+import com.mineinabyss.idofront.serialization.DoubleRangeSerializer
+import com.mineinabyss.idofront.util.DoubleRange
+import com.mineinabyss.idofront.util.randomOrMin
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.bukkit.entity.LivingEntity
@@ -15,10 +18,10 @@ import org.bukkit.entity.LivingEntity
 @Serializable
 @SerialName("geary:deal_damage")
 public data class DealDamageAction(
-    val damage: Double
+    val damage: @Serializable(with = DoubleRangeSerializer::class) DoubleRange
 ) : GearyAction() {
     override fun runOn(entity: GearyEntity): Boolean {
-        entity.toBukkit<LivingEntity>()?.damage(damage) ?: return false
+        entity.toBukkit<LivingEntity>()?.damage(damage.randomOrMin()) ?: return false
         return true
     }
 }
