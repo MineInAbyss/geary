cbdc0c0f9657ca91e898a95972d69266eccaa24d
Boy <sivertpaulsen2@gmail.com>
2021-04-18 22:47:56 +0200

    making it 10 times cleaner

--- spigot/src/main/kotlin/com/mineinabyss/geary/minecraft/actions/PlaceBlockAction.kt
+++ spigot/src/main/kotlin/com/mineinabyss/geary/minecraft/actions/PlaceBlockAction.kt
@@ -2,29 +2,30 @@ package com.mineinabyss.geary.minecraft.actions
 
 import com.mineinabyss.geary.ecs.api.actions.GearyAction
 import com.mineinabyss.geary.ecs.api.entities.GearyEntity
+import com.mineinabyss.geary.minecraft.components.PlayerComponent
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.bukkit.Material
+import org.bukkit.block.BlockFace
 
 /**
  * Summons a block at given location.
  *
  * @param at The location to spawn block at.
- * @param block The block to spawn.
+ * @param blockType The block to spawn.
+ * @param maxDistance Max distance block will spawn.
  */
 @Serializable
 @SerialName("geary:placeblock")
-
 public data class PlaceBlockAction(
     val at: ConfigurableLocation,
-    val block: Material,
+    val blockType: Material,
+    val maxDistance: Int = 4,
 ) : GearyAction() {
     override fun runOn(entity: GearyEntity): Boolean {
+        //TODO Replaces block instead of placing adjacent to it
         val loc = at.get(entity) ?: return false
-        //TODO Make it summon ontop/underneath/adjacent to block
-        //     aka where air. Not only when block is air.
-        if (loc.world.getBlockAt(loc).type == Material.AIR)
-            loc.world.getBlockAt(loc).type = block
+        loc.block.type = blockType
         return true
     }
 }
