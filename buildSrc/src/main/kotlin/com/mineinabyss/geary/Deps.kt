3af84803a1ba2f4721f738faa5834a84bf1e7a66
0ffz <dan.voznyy@gmail.com>
2021-05-05 23:29:52 -0400

    Update Kotlin 1.5.0 and Minecraft 1.16.5

--- buildSrc/src/main/kotlin/com/mineinabyss/geary/Deps.kt
+++ buildSrc/src/main/kotlin/com/mineinabyss/geary/Deps.kt
@@ -1,6 +1,6 @@
 package com.mineinabyss.geary
 
 object Deps {
-    const val kotlinVersion = "1.4.31"
-    const val serverVersion = "1.16.3-R0.1-SNAPSHOT"
+    const val kotlinVersion = "1.5.0"
+    const val serverVersion = "1.16.5-R0.1-SNAPSHOT"
 }
