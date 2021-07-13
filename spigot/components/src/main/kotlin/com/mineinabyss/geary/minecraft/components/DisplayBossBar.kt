7623f7498b7c7c0ee81f92b198642e6f92986005
AidanF86 <aidan.t.foley@gmail.com>
2021-07-12 20:07:27 -0400

    Fixed issues (hopefully)

--- spigot/components/src/main/kotlin/com/mineinabyss/geary/minecraft/components/DisplayBossBar.kt
+++ spigot/components/src/main/kotlin/com/mineinabyss/geary/minecraft/components/DisplayBossBar.kt
@@ -3,8 +3,11 @@ package com.mineinabyss.geary.minecraft.components
 import com.mineinabyss.geary.ecs.api.autoscan.AutoscanComponent
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+import org.bukkit.Bukkit
 import org.bukkit.boss.BarStyle
 import org.bukkit.boss.BarColor
+import org.bukkit.boss.BossBar
 
 /**
  * > geary:bossbar
@@ -19,4 +22,5 @@ public class DisplayBossBar(
     public val style: BarStyle,
     public val range: Double
 ) {
+    @Transient public val bossBar: BossBar = Bukkit.createBossBar("Boss Bar", color, style)
 }
--- spigot/components/src/main/kotlin/com/mineinabyss/geary/minecraft/systems/BossBarDisplaySystem.kt
+++ spigot/components/src/main/kotlin/com/mineinabyss/geary/minecraft/systems/BossBarDisplaySystem.kt
@@ -3,37 +3,33 @@ package com.mineinabyss.geary.minecraft.systems
 import com.mineinabyss.geary.ecs.api.systems.TickingSystem
 import com.mineinabyss.geary.ecs.engine.iteration.QueryResult
 import com.mineinabyss.geary.ecs.query.Query
+import com.mineinabyss.geary.minecraft.components.BukkitEntityType
 import com.mineinabyss.geary.minecraft.components.DisplayBossBar
 import org.bukkit.Bukkit
 import org.bukkit.Location
+import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
 
 /**
  * Handles displaying of boss bars.
  */
 public object BossBarDisplaySystem : TickingSystem(interval = 10) {
+    private val QueryResult.bossbar by get<DisplayBossBar>()
+    private val QueryResult.bukkitentity by get<BukkitEntity>()
+
     override fun QueryResult.tick() {
-        // for each entity which has DisplayBossBar component
-        DisplayBossBarQuery.apply {
-            forEach { result: QueryResult ->
-                val displayBossBar: DisplayBossBar = result.entity.get<DisplayBossBar>() ?: return
-                val bossBar = Bukkit.createBossBar("Boss Bar", displayBossBar.color, displayBossBar.style)
-                for(player in Bukkit.getOnlinePlayers()) {
-                    val playerLocation = player.getLocation()
-                    val location = result.entity.get<Location>() ?: return
-                    if(playerLocation.world.equals(location) && playerLocation.distance(location) <= displayBossBar.range) {
-                        bossBar.addPlayer(player)
-                    }
-                    else {
-                        bossBar.removePlayer(player)
-                    }
+        forEach { result: QueryResult ->
+            val location = bukkitentity.getLocation()
+
+            for(player in location.getNearbyPlayers(bossbar.range)) {
+                val playerLocation = player.getLocation()
+                if(playerLocation.world.equals(location) && playerLocation.distance(location) <= bossbar.range) {
+                    bossbar.bossBar.addPlayer(player)
+                }
+                else {
+                    bossbar.bossBar.removePlayer(player)
                 }
             }
-
         }
     }
-}
-
-public object DisplayBossBarQuery : Query() {
-    private val QueryResult.bossbar by get<DisplayBossBar>()
 }
