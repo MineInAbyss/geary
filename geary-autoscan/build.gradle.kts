6cae001ed4eb7803a921f772752d26f4e7a1d85d
0ffz <dan.voznyy@gmail.com>
2022-01-27 22:25:32 -0500

    Punlish autoscan module

--- geary-autoscan/build.gradle.kts
+++ geary-autoscan/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("geary.kotlin-conventions")
 //    kotlin("plugin.serialization")
-//    id("com.mineinabyss.conventions.publication")
+    id("com.mineinabyss.conventions.publication")
 //    id("com.mineinabyss.conventions.testing")
 }
 
