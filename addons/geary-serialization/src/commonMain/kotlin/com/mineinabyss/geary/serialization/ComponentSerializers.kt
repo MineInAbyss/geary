72ef047c22b2f813bbb8b1159dc3155b0fb20be1
Danielle Voznyy <dan.voznyy@gmail.com>
2023-12-31 16:20:15 -0500

    Add helper functions for getting serializers with a list of namespaces

--- addons/geary-serialization/src/commonMain/kotlin/com/mineinabyss/geary/serialization/ComponentSerializers.kt
+++ addons/geary-serialization/src/commonMain/kotlin/com/mineinabyss/geary/serialization/ComponentSerializers.kt
@@ -9,7 +9,7 @@ interface ComponentSerializers {
     val module: SerializersModule
 
     //TODO allow this to work for all registered classes, not just components
-    fun getClassFor(serialName: String): KClass<out Component>
+    fun getClassFor(serialName: String, namespaces: List<String> = listOf()): KClass<out Component>
 
     fun <T : Component> getSerializerFor(
         key: String,
@@ -18,5 +18,13 @@ interface ComponentSerializers {
 
     fun <T : Component> getSerializerFor(kClass: KClass<in T>): DeserializationStrategy<out T>?
     fun getSerialNameFor(kClass: KClass<out Component>): String?
+
+    companion object {
+        private val camelRegex = Regex("([A-Z])")
+        fun String.fromCamelCaseToSnakeCase(): String {
+            return this.replace(camelRegex, "_$1").removePrefix("_").lowercase()
+        }
+        fun String.hasNamespace(): Boolean = contains(":")
+    }
 }
 
