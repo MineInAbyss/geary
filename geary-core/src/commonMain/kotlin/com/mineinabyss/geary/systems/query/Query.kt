0970444aac08130a5b4c6d6356c0aff639f2cb55
Danielle Voznyy <dan.voznyy@gmail.com>
2024-02-17 11:27:37 -0500

    Start laying things out for better query syntax

--- geary-core/src/commonMain/kotlin/com/mineinabyss/geary/systems/query/Query.kt
+++ geary-core/src/commonMain/kotlin/com/mineinabyss/geary/systems/query/Query.kt
@@ -1,67 +1,24 @@
 package com.mineinabyss.geary.systems.query
 
-import com.mineinabyss.geary.datatypes.GearyEntity
 import com.mineinabyss.geary.datatypes.family.Family
+import com.mineinabyss.geary.datatypes.family.family
 import com.mineinabyss.geary.engine.archetypes.Archetype
-import com.mineinabyss.geary.helpers.fastForEachWithIndex
-import com.mineinabyss.geary.modules.geary
-import com.mineinabyss.geary.systems.accessors.AccessorHolder
+import com.mineinabyss.geary.systems.accessors.AccessorOperations
 import com.mineinabyss.geary.systems.accessors.FamilyMatching
-import com.mineinabyss.geary.systems.accessors.Pointer
-import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
-/**com.mineinabyss.geary.ecs.engine.iteration.accessors
- * @property matchedArchetypes A set of archetypes which have been matched to this query.
- */
-abstract class Query : AccessorHolder() {
+abstract class Query : AccessorOperations() {
     @PublishedApi
-    internal val matchedArchetypes: MutableSet<Archetype> = mutableSetOf()
-
+    internal var currArchetype: Archetype? = null
     @PublishedApi
-    internal var registered: Boolean = false
+    internal var currEntityIndex: Int = 0
 
-    val matchedEntities
-        get(): List<GearyEntity> {
-            registerIfNotRegistered()
-            return matchedArchetypes.flatMap { it.entities }
-        }
+    val target: QueriedEntity = TODO()
 
-    fun registerIfNotRegistered() {
-        if (!registered) {
-            geary.queryManager.trackQuery(this)
-        }
-    }
-
-    inline fun <T> toList(crossinline map: (Pointer) -> T): List<T> {
-        val list = mutableListOf<T>()
-        forEach { list.add(map(it)) }
-        return list
-    }
-
-    /**
-     * Quickly iterates over all matched entities, running [run] for each.
-     *
-     * Use [apply] on the query to use its accessors.
-     * */
-    inline fun forEach(crossinline run: (Pointer) -> Unit) {
-        registerIfNotRegistered()
-        val matched = matchedArchetypes.toList()
-        matched.fastForEachWithIndex { i, archetype ->
-            archetype.isIterating = true
-            val upTo = archetype.size
-            // TODO upTo isn't perfect for cases where entities may be added or removed in the same iteration
-            for (entityIndex in 0 until upTo) {
-                run(Pointer(archetype, entityIndex))
-            }
-            archetype.isIterating = false
-        }
-    }
-
-    operator fun Family.provideDelegate(thisRef: GearyQuery, property: KProperty<*>): ReadOnlyProperty<Any, Family> {
-        mutableFamily.add(this)
-        return ReadOnlyProperty { thisRef, prop ->
-            this@provideDelegate
+    private val props: MutableMap<KProperty<*>, Family> = mutableMapOf()
+    fun buildQuery(): Family = family {
+        for (family in props.values) {
+            add(family)
         }
     }
 
@@ -70,7 +27,7 @@ abstract class Query : AccessorHolder() {
         thisRef: Any,
         prop: KProperty<*>
     ): T {
-        family?.let { mutableFamily.add(it) }
+        family?.let { props[prop] = it }
         return this
     }
 }
