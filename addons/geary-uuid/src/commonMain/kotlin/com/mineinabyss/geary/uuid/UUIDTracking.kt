9b4a5c4ca4bef7d2bc6fce7afed88c894ad6d924
Danielle Voznyy <dan.voznyy@gmail.com>
2024-02-28 10:00:18 -0500

    Cleaning out some TODOs

--- addons/geary-uuid/src/commonMain/kotlin/com/mineinabyss/geary/uuid/UUIDTracking.kt
+++ addons/geary-uuid/src/commonMain/kotlin/com/mineinabyss/geary/uuid/UUIDTracking.kt
@@ -1,8 +1,8 @@
 package com.mineinabyss.geary.uuid
 
 import com.mineinabyss.geary.addons.dsl.GearyAddonWithDefault
-import com.mineinabyss.geary.uuid.systems.createTrackUUIDOnAddSystem
-import com.mineinabyss.geary.uuid.systems.createUntrackUuidOnRemoveSystem
+import com.mineinabyss.geary.uuid.systems.createTrackUUIDOnAddListener
+import com.mineinabyss.geary.uuid.systems.createUntrackUuidOnRemoveListener
 import com.mineinabyss.idofront.di.DI
 
 val uuid2Geary by DI.observe<UUID2GearyMap>()
@@ -11,7 +11,7 @@ object UUIDTracking : GearyAddonWithDefault<UUID2GearyMap> {
     override fun default() = SimpleUUID2GearyMap()
 
     override fun UUID2GearyMap.install() {
-        createTrackUUIDOnAddSystem()
-        createUntrackUuidOnRemoveSystem()
+        createTrackUUIDOnAddListener()
+        createUntrackUuidOnRemoveListener()
     }
 }
--- addons/geary-uuid/src/commonMain/kotlin/com/mineinabyss/geary/uuid/systems/TrackUuidOnAdd.kt
+++ addons/geary-uuid/src/commonMain/kotlin/com/mineinabyss/geary/uuid/systems/TrackUuidOnAdd.kt
@@ -2,15 +2,13 @@ package com.mineinabyss.geary.uuid.systems
 
 import com.benasher44.uuid.Uuid
 import com.benasher44.uuid.uuid4
-import com.mineinabyss.geary.annotations.optin.UnsafeAccessors
 import com.mineinabyss.geary.modules.geary
 import com.mineinabyss.geary.systems.builders.listener
 import com.mineinabyss.geary.systems.query.ListenerQuery
 import com.mineinabyss.geary.uuid.components.RegenerateUUIDOnClash
 import com.mineinabyss.geary.uuid.uuid2Geary
 
-@OptIn(UnsafeAccessors::class)
-fun createTrackUUIDOnAddSystem() = geary.listener(object : ListenerQuery() {
+fun createTrackUUIDOnAddListener() = geary.listener(object : ListenerQuery() {
     var uuid by get<Uuid>()
     val regenerateUUIDOnClash by get<RegenerateUUIDOnClash>().orNull()
     override fun ensure() = event.anySet(::uuid)
