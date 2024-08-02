d5c0906de3530a421952c57b04af643114f384f6
Danielle Voznyy <dan.voznyy@gmail.com>
2024-08-02 13:25:46 -0400

    feat: Start working on better inline expression support

---
+++ addons/geary-actions/src/commonMain/kotlin/com/mineinabyss/geary/actions/expressions/ExpressionEvaluator.kt
@@ -0,0 +1,10 @@
+package com.mineinabyss.geary.actions.expressions
+
+import com.mineinabyss.geary.actions.ActionGroupContext
+
+class ExpressionEvaluator {
+
+}
+// entity.location
+// test.nearbyEntities { a: string, b: 10 }
+// .filter(isType: [ minecraft:pig ])
